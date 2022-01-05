3cf90ba8b6ab11fed5ea8aca5712850cb5da0da4
Faryad Az Zim <73974353+faryadazim@users.noreply.github.com>
2022-01-05 13:00:57 +0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,20 @@
-var mongoose = require("mongoose")
+var mongoose = require("mongoose");
 function dbConnection() {
   mongoose.connect(
-    "mongodb+srv://jsguru:jsguru123@cluster0.7azk9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true },
+    "mongodb+srv://faryaduser:faryaduser123@cluster0.tfvhz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    },
     function (err) {
       if (err) {
-        console.log("error while connecting", err)
+        console.log("error while connecting", err);
       } else {
-        console.log("successfully connected")
+        console.log("successfully connected");
       }
     }
-  )
+  );
 }
 
-module.exports = dbConnection
+module.exports = dbConnection;
